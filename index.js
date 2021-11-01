4dd71a400a336f8fc5aa3b6ff0f674fd7a62def0
ElijahMinc <55488975+ElijahMinc@users.noreply.github.com>
2021-11-01 12:16:47 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const prevBtns = document.querySelectorAll(".btn-prev");
+const nextBtns = document.querySelectorAll(".btn-next");
+const progress = document.querySelector(".progress");
+const progressSteps = document.querySelectorAll(".progress-step");
+const formSteps = document.querySelectorAll(".form__step");
+
+let formStepsNum = 0;
+
+nextBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    formStepsNum++;
+    updateFormSteps();
+    updateProgressbar();
+  });
+});
+
+prevBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    formStepsNum--;
+    updateFormSteps();
+    updateProgressbar();
+  });
+});
+
+function updateFormSteps() {
+  formSteps.forEach((formStep) => {
+    formStep.classList.contains("form__step-active") &&
+      formStep.classList.remove("form__step-active");
+  });
+  formSteps[formStepsNum].classList.add("form__step-active");
+}
+
+function updateProgressbar() {
+  progressSteps.forEach((progressStep, idx) => {
+    if (idx < formStepsNum + 1) {
+      progressStep.classList.add("progress-step-active");
+    } else {
+      progressStep.classList.remove("progress-step-active");
+    }
+  });
+
+  const progressActive = document.querySelectorAll(".progress-step-active");
+
+  progress.style.width =
+    ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
+}
